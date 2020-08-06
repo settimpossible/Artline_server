14e7edcf255e6f0682eceec7983e2c85833e66a2
Setti <setti@leqoqo.com>
2020-08-07 01:49:53 +0200

    deploy first try

---
+++ public/precache-manifest.5f9f741616d4da580f79b0e0e7369f0f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18b4c20d4e48cdcbf05f4d4483d9a4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c560d5ba224556828753",
+    "url": "/static/css/main.6690ff7b.chunk.css"
+  },
+  {
+    "revision": "98eab31e0ee59eacb0e1",
+    "url": "/static/js/2.ce628480.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ce628480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c560d5ba224556828753",
+    "url": "/static/js/main.a7c307fa.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ed3698aed42dfbc1215bc976e7eaada8",
+    "url": "/static/media/Barlow-ExtraLight.ed3698ae.woff"
+  },
+  {
+    "revision": "7729f43a51547ae97e0da4a5d50d2be2",
+    "url": "/static/media/Barlow-Light.7729f43a.woff"
+  },
+  {
+    "revision": "02a9a93859fb5e1f3c8151cd95e29a8d",
+    "url": "/static/media/Barlow-Regular.02a9a938.woff"
+  },
+  {
+    "revision": "a8ee4641e93ad696e616477beec5c85d",
+    "url": "/static/media/SIFONN_PRO.a8ee4641.woff"
+  }
+]);
